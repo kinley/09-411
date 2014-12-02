19385d0b78e36635fead1b6f8f6f41f8920d5e7f
unknown <ADShamsutdinov@IVMIT-14-909-05.stud.kpfu.ru>
2014-12-02 12:42:14 +0400

    Shamsutdinov hw1-2

---
+++ homeworks/Shamsutdinov/2/main.cpp
@@ -0,0 +1,17 @@
+#include <cstdlib>
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main(int argc, char *argv[])
+{ while (1) {
+        (1);
+        (2);
+        (3);}
+        (4)
+        (5)
+        if (2) then (6)
+        while (3){(7)}
+    system("PAUSE");
+    return EXIT_SUCCESS;
+}
