8e4f21915e8b2c4dfd2439dd0f1049033c98e7df
Linar23 <skier2396@gmail.com>
2014-12-08 23:22:52 +0300

    Create pas3.pas

---
+++ homeworks/Husnutdinov/Homeworks-old/pas3.pas
@@ -0,0 +1,41 @@
+program zad1;
+var m,n,k,i:integer;
+begin
+read(n,k);
+
+if (n=0) or (k=0) then write(0) else
+
+if ((n<0) and (k>0)) then
+begin
+n:=-n;
+for i:=1 to n do
+m:=m+k;
+write(-m);
+end else
+
+
+if ((n>0) and (k<0)) then
+begin
+n:=-n;
+for i:=1 to n do
+m:=m+k;
+write(-m);
+end else
+
+if ((n>0) and (k>0)) then
+begin
+for i:=1 to n do
+m:=m+k;
+write(m);
+end else
+
+if ((n<0) and (k<0)) then
+begin
+k:=-k;
+n:=-n;
+for i:=1 to n do
+m:=m+k;
+write(m);
+end;
+
+end.
