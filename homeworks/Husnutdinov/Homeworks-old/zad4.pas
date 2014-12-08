f15c34d196b856db9fe4163299232e4f048e3d58
Linar23 <skier2396@gmail.com>
2014-12-08 23:24:49 +0300

    Create zad4.pas

---
+++ homeworks/Husnutdinov/Homeworks-old/zad4.pas
@@ -0,0 +1,54 @@
+program zad1;
+var m,n,k,i,prov:integer;
+begin
+read(m,n);
+
+if (m=0) then begin write(0,' ',0); prov:=1; end;
+if prov<>1 then
+if (n=0) then begin write('Error'); prov:=1; end;
+
+if prov<>1 then
+if ((m>0) and (n>0) and (m>n)) then
+begin
+while m>=0 do
+begin
+m:=m-n;
+i:=i+1;
+end;
+write(i-1,' ',m+n);
+prov:=1;
+end;
+
+if prov<>1 then
+if ((m<0) and (n>0) and (m>n)) then
+begin
+m:=-m;
+while m>=0 do
+begin
+m:=m-n;
+i:=i+1;
+end;
+write(i-1,' ',-(m+n));
+prov:=1;
+end;
+
+if prov<>1 then
+if ((m>0) and (n<0) and (m>n)) then
+begin
+m:=-m;
+while m>=0 do
+begin
+m:=m-n;
+i:=i+1;
+end;
+write(i-1,' ',-(m+n));
+prov:=1;
+end;
+
+if prov<>1 then
+if  m<n then
+begin
+write(m,' ',0);
+end;
+
+end.
