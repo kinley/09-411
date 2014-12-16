81ae62656a69ebcc52c71d3d982994ec75d9df45
hibace <yalkaevnikolai@gmail.com>
2014-12-16 10:57:11 +0300

    update

--- homeworks/Yalkaev/7-18-9b/9b.pas
+++ homeworks/Yalkaev/7-18-9b/9b.pas
@@ -1,11 +1,12 @@
 var
-   x,y:real;
+  x, y: real;
+
 begin
-write('Vvedite x: ');
-read(x);
-y:=sqr(sin(x))+1;
-y:=sqrt(y);
-y:=y*arctan(abs(x)+1);
-writeln('Y = ', y);
-readln;
+  write('Vvedite x: ');
+  read(x);
+  y := sqr(sin(x)) + 1;
+  y := sqrt(y);
+  y := y * arctan(abs(x) + 1);
+  writeln('Y = ', y);
+  readln;
 end.
