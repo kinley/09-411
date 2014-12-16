0aac8b03a6101cd3fa017ea47739ea780d433eaf
mukhamediarov <mukhamediarov3.14@gmail.com>
2014-12-17 00:32:22 +0400

    Update vichislenie_cos_x.pas

--- homeworks/Mukhamediarov/vichislenie_cos_x.pas
+++ homeworks/Mukhamediarov/vichislenie_cos_x.pas
@@ -14,3 +14,22 @@ writeln(y);
 writeln(cos(x));
 c2:
 end.
+
+
+ISPRAVLENIE CODA
+
+begin
+ var 
+ x,y:real;
+  readln(x);
+  if (x<=0) or (x>=pi) then write('SYSTEM FATAL SUPER PUPER ERROR')
+  else 
+     begin
+      y:=1-(x*x)/2;
+      y:=y+(x*x*x*x)/(2*3*4);
+      y:=y-(x*x*x*x*x*x)/(2*3*4*5*6);
+      y:=y+(x*x*x*x*x*x*x*x)/(2*3*4*5*6*7*8);
+        writeln('PRIMERNOE ZNACHENIE COSINUSA= ',y);
+        writeln('ZNACHENIE COSINUSA= ',cos(x));
+     end;  
+end.
