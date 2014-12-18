8abf93badd6723e35c64df294f3b351a3c46348d
hibace <yalkaevnikolai@gmail.com>
2014-12-18 16:59:47 +0300

    F2

---
+++ exam/F2.pas
@@ -0,0 +1,19 @@
+const
+  fName = 'File.txt';
+
+var
+  F: Text;
+  N, S: Integer;
+
+begin
+  Assign(F, fName);
+  Reset(F);
+  S := 0;
+  while not EOF(F) do
+  begin
+    Read(F, N);
+    S += N;
+  end;
+  Close(F);
+  WriteLn(S);
+end.
