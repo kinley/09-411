d9205c8b917aaf70606eb1a1d18822746e8ee282
MaxMannanov <mmannanov@gmail.com>
2014-12-18 09:19:44 +0300

    Create task4a.pas

---
+++ homeworks/Mannanov/2.12.2014/task4a.pas
@@ -0,0 +1,18 @@
+var
+ h:real;
+ n:integer;
+function Hn(m:integer):real;
+var
+ i:integer;
+ g:real;
+begin
+  g:=0;
+  for i:=1 to m do
+   g:=g+1/i;
+  Hn:=g; 
+end;
+begin
+readln(n);
+h:=Hn(n);
+writeln(h);
+end.  
