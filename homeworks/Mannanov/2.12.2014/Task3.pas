7a1979fde368d2b91e0ed0a124a0908b48e4abf0
MaxMannanov <mmannanov@gmail.com>
2014-12-18 09:50:10 +0300

    Right codestyle

--- homeworks/Mannanov/2.12.2014/Task3.pas
+++ homeworks/Mannanov/2.12.2014/Task3.pas
@@ -1,12 +1,13 @@
 var
-x,y:real;
+  x, y: real;
+
 begin
-writeln('Введите ваше X');
-readln(x);
-x:=abs(x);
-y:=1+cos(x);
-y:=y/(x+1);
-y:=ln(y)*exp(x);
-writeln('Получите ваше Y');
-writeln(y);
+  writeln('Введите ваше X');
+  readln(x);
+  x := abs(x);
+  y := 1 + cos(x);
+  y := y / (x + 1);
+  y := ln(y) * exp(x);
+  writeln('Получите ваше Y');
+  writeln(y);
 end.
