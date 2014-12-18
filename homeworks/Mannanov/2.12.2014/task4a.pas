b6f98e3e27fe1e9c4d2d4ae42e32204393f641fa
MaxMannanov <mmannanov@gmail.com>
2014-12-18 09:47:53 +0300

    Right codestyle

--- homeworks/Mannanov/2.12.2014/task4a.pas
+++ homeworks/Mannanov/2.12.2014/task4a.pas
@@ -1,18 +1,20 @@
 var
- h:real;
- n:integer;
-function Hn(m:integer):real;
+  h: real;
+  n: integer;
+
+function Hn(m: integer): real;
 var
- i:integer;
- g:real;
+  i: integer;
+  g: real;
 begin
-  g:=0;
-  for i:=1 to m do
-   g:=g+1/i;
-  Hn:=g; 
+  g := 0;
+  for i := 1 to m do
+    g := g + 1 / i;
+  Hn := g; 
 end;
+
 begin
-readln(n);
-h:=Hn(n);
-writeln(h);
-end.  
+  readln(n);
+  h := Hn(n);
+  writeln(h);
+end.
