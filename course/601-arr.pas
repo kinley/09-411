49ac86ae79d2659b50cb667640e83c3e89e2a8b0
Anvar Tuykin <tuykin@gmail.com>
2014-12-18 11:32:35 +0300

    Add funcs and procedures for array (601-arr)

--- course/601-arr.pas
+++ course/601-arr.pas
@@ -1,10 +1,38 @@
 const
   N = 5;
+type
+  T = 1..N;
+  ArrayType = Array[T] of Integer;
 var
   i,j: Integer;
   arr: Array[1..N] of Integer;
-  matrix: Array[1..N] of Array[1..N] of Integer;
+  matrix: Array[1..N] of Array[T] of Integer;
   arrN: Array[1..N, 2..5, 0..9] of Integer;
+  a: ArrayType;
+
+function ReadArray: ArrayType;
+var
+  i: Integer;
+  arr: ArrayType;
+begin
+  writeln('Enter array elements: ');
+  for i := 1 to N do
+  begin
+    read(arr[i]);
+  end;
+  ReadArray := arr;
+end;
+
+procedure PrintArray(arr: ArrayType);
+var
+  i: Integer;
+begin
+  for i := 1 to length(arr) do
+  begin
+    write(arr[i], ' ')
+  end;
+  writeln;
+end;
 
 BEGIN
   for i := 1 to N do
@@ -27,4 +55,7 @@ BEGIN
   end;
 
   writeln(Length(arrN));
+
+  a := ReadArray();
+  PrintArray(a);
 END.
