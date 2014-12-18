e085f582c0bb5968eaa66e7ae432c73fecb20a1a
MaxMannanov <mmannanov@gmail.com>
2014-12-18 09:54:33 +0300

    Right codestyle

--- exam/r1.pas
+++ exam/r1.pas
@@ -1,19 +1,24 @@
-const n=3;
-type Example=record
- chislitel:integer;
- znamenatel:integer;
-end; 
-var a:array[1..n] of Example;
-x,s:real; 
-i:integer;
+const
+  n = 3;//количество аргументов в схеме.Взято для удобства,может быть изменено
+
+type
+  Example = record
+    chislitel: integer;
+    znamenatel: integer;
+  end;
+
+var
+  a: array[1..n] of Example;
+  x, s: real; 
+  i: integer;
 
 begin
-read(x);
-for i:=1 to n do
-readln(a[i].chislitel,a[i].znamenatel);
-s:=0;
-for i:=n downto 1 do
-  s:=a[i].chislitel/a[i].znamenatel+s*x;
-writeln(s)
-end.  
+  read(x);
+  for i := 1 to n do
+    readln(a[i].chislitel, a[i].znamenatel);
+  s := 0;
+  for i := n downto 1 do
+    s := a[i].chislitel / a[i].znamenatel + s * x;
+  writeln(s)
+end.
   
