567f65145e7c0e283c2f3309076b24d83d87722f
Ran1s <sinar2008@yandex.ru>
2014-12-23 02:35:44 +0300

    Update 3_mAndN.pas

--- homeworks/Nigmatullin/basics/3_mAndN.pas
+++ homeworks/Nigmatullin/basics/3_mAndN.pas
@@ -1,25 +1,39 @@
-program mn;
+program mAndN;
+
+{$APPTYPE CONSOLE}
 var
-        m, m1, n, k, i : integer;
+  m, n, k, i : integer;
+
 begin
-        readln(m, n, k);
-        if(n > 0) and ((k > 0) or (k < 0)) then
-        begin
-                for i := 1 to n do
-                        m1 := m1 + k;
-        end
-        else if (n < 0) and (k > 0) then
-        begin
-                for i := 1 to k do
-                        m1 := m1 + n;
-        end
-        else if(n < 0) and (k < 0) then
-        begin
-                for i := 1 to n *(-1) do
-                begin
-                        m1 := m1 - k;
-                end;
-        end;
-        writeln(m1);
-        readln;
+  readln(n, k);
+  m := 0;
+  if (n > 0) then
+  begin
+
+    for i := 1 to n do
+    begin
+      m := m + k;
+    end;
+
+  end
+  else if (k > 0) then
+  begin
+
+    for i := 1 to k do
+    begin
+      m := m + n;
+    end;
+
+  end
+  else if (k < 0) then
+  begin
+
+    for i := -1 downto k do
+    begin
+      m := m - n;
+    end;
+
+  end;
+
+  Writeln(m);
 end.
