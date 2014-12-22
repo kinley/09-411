3457c5e3dac5e96be38ea975418ab4d8f2999c71
Milla4815162342 <lo4815162342st.108@gmail.com>
2014-12-22 23:55:48 +0300

    Create 14.pas

---
+++ homeworks/Samitova/14.pas
@@ -0,0 +1,21 @@
+Begin
+ If S1 then
+  begin
+   p1;
+   if S3 then 
+    begin
+     p3;
+     p4;
+    end;
+   else
+    if S4 then
+     p5;
+   end;
+  else
+   if S2 then
+    begin
+     p2;
+     if S5 then
+      p6;
+    end;
+End.
