c1b43ae447cf3a7bbf6d1b5e85499d4ee1487313
Ran1s <sinar2008@yandex.ru>
2014-12-23 04:36:53 +0300

    Update 4_mDivN.pas

--- homeworks/Nigmatullin/basics/4_mDivN.pas
+++ homeworks/Nigmatullin/basics/4_mDivN.pas
@@ -1,50 +1,58 @@
 program mDivN;
+{$APPTYPE CONSOLE}
 var
-        i, m, n : integer;
-        //m, n : real;
+  i, m, n : integer;
 begin
-        readln(m, n);
-        if n = 0 then
-        begin
-            writeln('error');
-            exit
-        end;
-        if (m > 0) and (n > 0) then
-        begin
-                while (m >= n) do
-                begin
-                        m := m - n;
-                        inc(i);
-                end;
-        end
-	    else if (m < 0) and (n < 0) then
-        begin
-                while (m <= n) do
-                begin
-                        m := m - n;
-                        inc(i);
-                end;
+  readln(m, n);
 
-        end
-        else if (m > 0) and (n < 0) then
-        begin
-                while (m >= n * (-1)) do
-                begin
-                        m := m + n;
-                        inc(i);
-                end;
-                i := i * (-1);
-        end
-        else if (m < 0) and (n > 0) then
-        begin
-                while(m * (-1) >= n) do
-                begin
-                        m := m + n;
-                        inc(i);
-                end;
-                i := i *(-1);
-        end;
+  if n = 0 then
+  begin
+    writeln('error');
+    exit
+  end;
 
-        writeln(i, '  ', m);
-        readln;
+  i := 0;
+
+  if (m > 0) and (n > 0) then
+  begin
+
+    while (m >= n) do
+    begin
+      m := m - n;
+      inc(i);
+    end;
+
+  end
+  else if (m < 0) and (n < 0) then
+  begin
+
+    while (m <= n) do
+    begin
+      m := m - n;
+      inc(i);
+    end;
+
+  end
+  else if (m > 0) and (n < 0) then
+  begin
+
+    while (m >= n * (-1)) do
+    begin
+      m := m + n;
+      dec(i);
+    end;
+
+  end
+  else if (m < 0) and (n > 0) then
+  begin
+
+    while(m * (-1) >= n) do
+    begin
+      m := m + n;
+      dec(i);
+    end;
+
+  end;
+
+  writeln(i, '  ', m);
 end.
