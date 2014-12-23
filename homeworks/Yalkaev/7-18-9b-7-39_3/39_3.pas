64076ca0719b0529ae4e878dfb7d5a1ed4e99d72
Nikolay <yalkaevnikolai@gmail.com>
2014-12-23 11:14:16 +0300

    code

--- homeworks/Yalkaev/7-18-9b-7-39_3/39_3.pas
+++ homeworks/Yalkaev/7-18-9b-7-39_3/39_3.pas
@@ -1,6 +1,4 @@
 // c=альфа, d=бета. По условию: альфа>0, бета<1.
-// Возможно, я не совсем правильно понял условие, связанное с ε.
-// С интегралами, зависящими от параметров, встретился в первый раз.
 
 program integral_39_3;
 
@@ -11,10 +9,25 @@ const
   e = 0.0001;
 
 var
-  ik, h, y, x, c, d: real;
+  ik, h, y, x, c, d, fK, S, P: real;
   i, k: integer;
 
 begin
+  fK := 0;
+  ik := 1;
+  k := 1;
+  for k := 1 to N do
+  begin
+    S := 2 * ik;
+  end;
+  for k := k + 1 to N do
+  begin
+    P := 2 * ik;
+  end;
+  fK := abs(S - P);
+  if fK > e then inc(k)
+  else writeln(ik);
+  
   write('Введите c (c>0): ');
   readln(c);
   
@@ -34,7 +47,5 @@ begin
     ik := ik + y * h;
   end;
   
-  writeln('При n=4: I=', ik:0:5);
-  ik := ik * e;
-  writeln('С учетом погрешности ε: I=', ik:0:15);
+  writeln('При n=4: I=', ik:0:10);
 end.
