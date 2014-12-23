b407f09154dfc346592a91081f6cbc30a44ac461
mukhamediarov <mukhamediarov3.14@gmail.com>
2014-12-23 10:53:04 +0300

    Update 1glava_4B.pas

--- homeworks/Mukhamediarov/1glava_4B.pas
+++ homeworks/Mukhamediarov/1glava_4B.pas
@@ -1,17 +1,17 @@
 var
-x:longint;
-k:integer;
+  x: longint;
+  k: integer;
 
 begin
-k := 0;
-write('vvedite chislo x= ');
-readln(x);
-
-while x > 0 do 
-begin
-  x := x div 10;
-  k := k + 1;
-end;
-
-writeln('kol-vo cifr v etom chisle= ',k);
+  k := 0;
+  write('vvedite chislo x= ');
+  readln(x);
+  
+  while x > 0 do 
+  begin
+    x := x div 10;
+    k := k + 1;
+  end;
+  
+  writeln('kol-vo cifr v etom chisle= ', k);
 end.
