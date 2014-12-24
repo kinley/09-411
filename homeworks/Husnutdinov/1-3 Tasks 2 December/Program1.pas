7fa8d8e926926f0ff61b928305b5726a5dc21dfb
Linar23 <skier2396@gmail.com>
2014-12-24 21:16:30 +0300

    Update Program1.pas

--- homeworks/Husnutdinov/1-3 Tasks 2 December/Program1.pas
+++ homeworks/Husnutdinov/1-3 Tasks 2 December/Program1.pas
@@ -1,13 +1,16 @@
-program zad3;
-var u,v,w,x,y:real;
+program zad3b;
+
+var
+  u, v, w, x, y: real;
+
 begin
 read(x);
-u:=power(x,2);
-v:=power(x,3);
-w:=power(x,5);
+u := x*x;
+v := x*x*x;
+w := x*x*x*x*x;
 
-y:=(w+5.7*v);
-y:=y/(u-7.5);
-y:=y+4.2*w-8.9*u;
+y := (w + 5.7 * v);
+y := y / (u - 7.5);
+y := y + 4.2 * w - 8.9 * u;
 write(y:3:2);
 end.
