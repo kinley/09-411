3dad835e3376d535ecb2f0bce259dad5891aae17
Linar23 <skier2396@gmail.com>
2014-12-25 01:52:24 +0300

    Update zad51.pas

--- homeworks/Husnutdinov/Homeworks-old/zad51.pas
+++ homeworks/Husnutdinov/Homeworks-old/zad51.pas
@@ -1,13 +1,17 @@
 program zad51;
-var m,k:integer;
-p,n:real;
+
+var
+  m, k: integer;
+  p, n: real;
+
 begin
-read(m,n);
-k:=0;
-repeat
-p:=round(power(n,k));
-k:=k+1;
-until p>m; 
-k:=round(ln(p)/ln(n));
-write(k);
+  {Opredelyem min k takoe chto m<n^k}
+  read(m, n);
+  k := 0;
+  repeat
+    p := round(power(n, k));
+    k := k + 1;
+  until p > m;
+  k := round(ln(p) / ln(n));
+  write(k);
 end.
