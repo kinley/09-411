78e595c787cc63d36fbfdffe4e953eae612fc8e6
Linar23 <skier2396@gmail.com>
2014-12-25 09:27:40 +0300

    Update chapter4(task15).pas

--- homeworks/Husnutdinov/New-homeworks/chapter4(task15).pas
+++ homeworks/Husnutdinov/New-homeworks/chapter4(task15).pas
@@ -1,24 +1,19 @@
 program chapter4(task15);
 
 var
-  k, n, i, count1, param, count2: integer;
+  k, n, i, count1, param, count2, m: integer;
 
 begin
-  
   writeln('Enter the numbers in sequence');
-  write(1, ' ');
-  
-  read(k, n);
-  
+  read(i, k, n);
   {Read the number and at the same time determine the status of the interval}
-  for i := 1 to n do
+  for m := 1 to n do
   begin
     read(param);
-    if (param > 1) and (param <= k) then count1 := count1 + 1;
+    if (param > i) and (param <= k) then count1 := count1 + 1;
     if (param mod k = 1) or (param mod k = 2) or (param mod k = 3) then count2 := count2 + 1;
     param := 0;
   end;
-  
   writeln('First condition ', count1);
   write('Second condition ', count2);
 end.
