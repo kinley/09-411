429a0c1dd3a3f5f15d9b37b27ebb32da4d786950
Picikak03 <Picikak03@gmail.com>
2014-12-25 10:11:38 +0300

    maybe this?

--- homeworks/Nikolaev/13-22-8c/III-8c.pas
+++ homeworks/Nikolaev/13-22-8c/III-8c.pas
@@ -1,4 +1,4 @@
-var y,x:real;
+var y,x,z:real;
 	i:integer;
 
 function step(x:real; n:integer):real;
@@ -8,13 +8,23 @@ begin
 	for i:=1 to n do step:=step*x;
 end;
 
+procedure readx(var x:real);
 begin
 	repeat
 		write('x=');
 		readln(x);
 		if (x<0)or(x>0.5) then writeln('	0<=x<=0.5 (!!!)');
 	until (x>=0)and(x<=0.5);
+end;
+
+begin
+	readx(x);
+	y:=0;
 	for i:=1 to 5 do
-		y:=y-step(-1,i)*step(x,i)/i;
+	begin
+		z:=step(x,i)/i;
+		z:=step(-1,i)*z;
+		y:=y-z
+	end;
 	writeln('y=',y:0:7);
 end.
