91106cf7d8124cf281f7edadabe8af0d5611cb9d
Milla4815162342 <lo4815162342st.108@gmail.com>
2014-12-25 09:23:58 +0300

    Update 10.g.pas

--- homeworks/Samitova/10.g.pas
+++ homeworks/Samitova/10.g.pas
@@ -28,3 +28,32 @@ begin
   end;
   Write(S4);
 end.
+{или}
+program pr;
+
+function fact(x: real): real;
+begin
+  if x = 0 then 
+    fact := 1
+  else 
+    fact := fact(x - 1) * x;
+end;
+
+var
+  x,n,k: integer;
+  k1, S4: real;
+
+begin
+  Read(x);
+  n:=2;
+  k := 1;
+  S4 := x / k;
+  for k := 2 to 2 * n do
+  begin
+    x := x * x;
+    k1 := k * k;
+    k1 := fact(k);
+    S4 := S4 + x / k1;
+  end;
+  Write(S4);
+end.
