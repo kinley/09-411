3a785f9798f6346578eaa7fa2f5757f0783cbdd3
Linar23 <skier2396@gmail.com>
2014-12-27 17:41:46 +0300

    Update chapter4(task38_1).pas

--- homeworks/Husnutdinov/New-homeworks/chapter4(task38_1).pas
+++ homeworks/Husnutdinov/New-homeworks/chapter4(task38_1).pas
@@ -5,51 +5,35 @@ var
   Ef: real;
 
 {Сounting factorial function}
-function factorial(s: integer): integer;
-var
-  l, rez1: integer;
+function fact(n: integer): longint;
 begin
-  rez1 := 1;
-  
-  for l := 2 to s do
-    rez1 := rez1 * l;
-  
+  if (n = 1) or (n = 0) then fact := 1
+  else fact := fact(n - 1) * n;         
 end;
 
 {Сounting formula function}
 function formula1(x: integer): real;
 var
-  rez: real;
-  i, m: integer;
+  i: integer;
 begin
-  m := -1;
   for i := 0 to x do
   begin
-    m := m + 1;
-    rez := rez + (power((x - m), m) * factorial(x - m)) / factorial(x);
+    if i = 0 then formula1 := 1 else formula1 := formula1(i - 1) + (power((x - i), i) * fact(x - i)) / fact(x);
   end;
 end;
 
 {Сounting emprirc formula function}
-function formula2(x1: integer): real;
-var
-  rez2: real;
+function formula2(x: integer): real;
 begin
-  
-  rez2 := sqrt(Pi * x1 / 2) - (2 / 3) + (11 / 24) * sqrt(Pi / (2 * x1)) + 4 / (135 * x1) - (71 / 1152) * sqrt(Pi / (2 * x1 * x1 * x1));
-  
+  formula2 := sqrt(Pi * x / 2) - (2 / 3) + (11 / 24) * sqrt(Pi / (2 * x)) + 4 / (135 * x) - (71 / 1152) * sqrt(Pi / (2 * x * x * x));
 end;
 
-
-
 begin
-  
   for i := 1 to 50 do
   begin
     Ef := abs(formula1(i) - formula2(i));
+    writeln(Ef:3:2);
     if Ef = 0 then param := param + 1;
     Ef := 0;
   end;
-  
-  if param = 50 then write('Empiric fucntion is true');
 end.
