ef6fb5b8e50f65b9199169531bedb8fd4b37ec43
Vasiliy <vasya.galeev@gmail.com>
2014-12-27 14:54:11 +0300

    Remade_task4

--- homeworks/Galeev/2_23_8d_30/Thitry(A).pas
+++ homeworks/Galeev/2_23_8d_30/Thitry(A).pas
@@ -4,17 +4,17 @@ const
     E = 0.0001;
     
 var
-  a, b, X, Xo, pastX, currentX, ksi, p: real;
+  a, b, Xo, pastX, currentX, ksi, p: real;
   i: integer;
   
 function f(c: real): real;
 begin
-  result := sqr(sqr(c)) * c - c - 0.02;
+  result := c * c * c * c * c - c - 0.02;
 end;
 
 function f_pr1(c: real) : real;
 begin
-  result := 5 * sqr(sqr(c)) - 1;
+  result := 5 * c * c * c * c - 1;
 end;
 
 function f_pr2(c: real): real;
@@ -22,26 +22,44 @@ begin
   result := 20 * sqr(c) * c;
 end;
 
+function search_Xo(c: real): real;
+begin
+  result := f(c) * f_pr2(c);
+end;
+
+function search_answer(c: real): real;
+begin
+  result := c - f(c) / f_pr1(c);
+end;
+
+function answer_cindition_isOK(c, d: real): boolean;
+begin
+  if ((abs(c - d) <= E) and (f(c) < 0.0001)) then
+    answer_cindition_isOK := true
+  else
+    answer_cindition_isOK := false;
+end;
+
 BEGIN
   read(a, b);
-  if (f(a) * f_pr2(a)) > 0 then
-    Xo := a;
-  if (f(b) * f_pr2(b)) > 0 then
+  if search_Xo(a) > 0 then
+    Xo := a
+  else
+  if search_Xo(b) > 0 then
     Xo := b;
-  X := Xo - f(Xo) / f_pr1(Xo);
+  currentX := search_answer(Xo);
   pastX := Xo;
-  currentX := X;
   i := 1;
   writeln('Xo = ', Xo);
-  if ((abs(currentX - pastX) <= E) and (f(currentX) < 0.0001)) then
+  if answer_cindition_isOK(currentX, pastX) then
     ksi := currentX
   else
     repeat
       p := pastX;
       pastX := currentX;
-      currentX := p - f(p) / f_pr1(p);
+      currentX := search_answer(p);
       inc(i)
-    until ((abs(currentX - pastX) <= E) and (f(currentX) < 0.0001));
+    until answer_cindition_isOK(currentX, pastX);
   ksi := currentX;
   writeln('Ksi = ', ksi:0:4);
   write('i = ', i);
