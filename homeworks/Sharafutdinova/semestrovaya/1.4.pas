6d40cd42d5c0d4dede38f1201016274b26236b95
Алсу <salsou96@mail.ru>
2015-03-22 16:15:06 +0500

    Update 1.4.pas

--- homeworks/Sharafutdinova/semestrovaya/1.4.pas
+++ homeworks/Sharafutdinova/semestrovaya/1.4.pas
@@ -5,6 +5,28 @@ const
 type
   arr = array[-2..n] of real;
   arrint = array [0..r - 1] of integer;
+  
+function dvoichnoe(a: real): real;
+//перевод числа в двоичную С.С.
+var
+  st, b, z: real;
+begin
+  st := 1;
+  if a < 0 then begin
+    a := -a;
+    z := -1;
+  end;
+  if a < 1 then 
+    while a > 0 do 
+    begin
+      a := a * 2;
+      b := int(a); 
+      a := a - b;
+      st := st / 10;
+      result := result + b * st;
+    end else result := 1;
+  if z = -1 then result := -result;    
+end; 
 
 function vichislenieX(x, y, z, a: real): real;
 var
@@ -12,11 +34,12 @@ var
   i: integer;
 begin
   f := (sin(x - y) - cos(z - a)) / 2;
-  f := f * 10;
+  f := dvoichnoe(f);
   t := 1 / 2;
   c := 0;
-  for i := 1 to 10 do 
+  for i := 1 to 10 do //преобразование числа по правилу Т
   begin
+    f := f * 10;
     b := int(f);
     f := f - b;
     if i = 10 then b := b / 2 else begin
@@ -26,10 +49,11 @@ begin
     end;
     c := c + b;
   end;
-  vichislenieX := c;
+  result := c;
 end;
 
 procedure polyinterval(r: integer; x: arr; var e: arrint);
+//определяет принадлежность Х интервалу
 var
   a1, a2: real;
   i, k: integer;
@@ -56,14 +80,9 @@ begin
   for a := 0 to 1 do 
   begin
     for i := 1 to n do 
-    begin
       x[i] := vichislenieX(x[i - 1], x[i - 2], x[i - 3], a);
-    end;
     polyinterval(R, x, e);
-    for i := 0 to r - 1 do 
-    begin
-      writeln(e[i]);
-    end;
+    for i := 0 to r - 1 do writeln(e[i]);
     for i := 0 to r - 1 do
       M := M + ((2 * i + 1) * e[i]) / (2 * r * n);
     for i := 0 to r - 1 do
