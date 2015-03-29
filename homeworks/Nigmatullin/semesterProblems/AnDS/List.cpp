fecd848182f0f8a06f04ad767db20d38f489201d
Ran1s <sinar2008@yandex.ru>
2015-03-30 02:46:00 +0300

    Update List.cpp

--- homeworks/Nigmatullin/semesterProblems/AnDS/List.cpp
+++ homeworks/Nigmatullin/semesterProblems/AnDS/List.cpp
@@ -1,6 +1,4 @@
 #include "List.h"
-
-
 template<typename T> pElement<T>::pElement(pElement<T> *n, T el)
 {
 	data = el;
@@ -29,69 +27,70 @@ template<typename T> List<T>::List()
 	size = 0;
 }
 
-template <typename T> void append(List<T> &list, T el)
+template<typename T> void List<T>::append(T el)
 {
-	if (list.head == NULL)
+	if (head == NULL)
 	{
-		list.head = new pElement<T>(el);
-		list.tail = list.head;
+		head = new pElement<T>(el);
+		tail = head;
 	}
 	else
 	{
-		list.tail->next = new pElement<T>(el);
-		list.tail = list.tail->next;
+		tail->next = new pElement<T>(el);
+		tail = tail->next;
 	}
-	list.size++;
+	size++;
 }
 
-template <typename T> void prepend(List<T> &list, T el)
+
+template<typename T> void List<T>::prepend(T el)
 {
-	if (list.head == NULL)
+	if (head == NULL)
 	{
-		pElement<T> *temp = new pElement<T>(list.head, el);
-		list.head = temp;
-		list.tail = list.head;
+		pElement<T> *temp = new pElement<T>(head, el);
+		head = temp;
+		tail = head;
 	}
 	else
 	{
-		pElement<T> *temp = new pElement<T>(list.head, el);
-		list.head = temp;
+		pElement<T> *temp = new pElement<T>(head, el);
+		head = temp;
 	}
-	list.size++;
+	size++;
 }
 
-template <typename T> bool del(List<T> &list, int position)
+template<typename T> bool List<T>::del(int position)
 {
-	if (position > list.size - 1)
+	if (position > size - 1)
 	{
 		return false;
 	}
 	if (position == 0)
 	{
-		if (list.head != NULL)
+		if (head != NULL)
 		{
-			pElement *temp = list.head;
-			list.head = list.head->next;
+			pElement *temp = head;
+			head = head->next;
 			delete(temp);
 			return true;
 		}
 		else
 		{
 			return false;
-		}		
+		}
 	}
-	else if (position == list.size - 1)
+	else if (position == size - 1)
 	{
-		if (list.tail != NULL)
+		if (tail != NULL)
 		{
-			pElement *temp = list.head;
+			pElement *temp = head;
 			for (int i = 0; i < position - 1; i++)
 			{
 				temp = temp->next;
 			}
 			delete(temp->next);
 			temp->next = NULL;
-			list.tail = temp;
+			tail = temp;
 			return true;
 		}
 		else
@@ -101,7 +100,7 @@ template <typename T> bool del(List<T> &list, int position)
 	}
 	else
 	{
-		pElement *temp = list.head;
+		pElement *temp = head;
 		for (int i = 0; i < position - 1; i++)
 		{
 			temp = temp->next;
@@ -113,15 +112,15 @@ template <typename T> bool del(List<T> &list, int position)
 		return true;
 	}
 }
-template <typename T> bool isEmpty(List<T> &list)
+template<typename T> bool List<T>::isEmpty()
 {
-	return (list.head == NULL);
+	return (head == NULL);
 }
 
-template <typename T> int size(List<T> &list)
+template<typename T> int List<T>::Size()
 {
 	int i = 0;
-	pElement<T> *temp = list.head;
+	pElement<T> *temp = head;
 	while (temp != NULL)
 	{
 		i++;
@@ -130,46 +129,42 @@ template <typename T> int size(List<T> &list)
 	return i;
 }
 
-template <typename T> bool insertElement(List<T> &list, int position, T el)
+template<typename T> bool List<T>::insertElement(int position, T el)
 {
-	if (position > list.size)
+	if (position > size)
 	{
 		return false;
 	}
 	if (position == 0)
 	{
-		if (list.head == NULL)
+		if (head == NULL)
 		{
-			list.head = new pElement<T>(el);
-			list.tail = list.tail;
-			list.size++;
+			head = new pElement<T>(el);
+			tail = tail;
 			return true;
 		}
 		else
 		{
-			pElement<T> *temp = new pElement<T>(list.head, el);
-			list.head = temp;
-			list.size++;
+			pElement<T> *temp = new pElement<T>(head, el);
+			head = temp;
 			return true;
 		}
 	}
-	else if (position == list.size)
+	else if (position == size)
 	{
 		pElement<T> *temp = new pElement<T>(el);
-		list.tail->next = temp;
-		list.tail = list.tail->next;
-		list.size++;
+		tail->next = temp;
+		tail = tail->next;
 		return true;
 	}
 	else
 	{
-		pElement<T> *temp = list.head;
+		pElement<T> *temp = head;
 		for (int i = 0; i < position - 1; i++)
 		{
 			temp = temp->next;
 		}
 		temp->next = new pElement<T>(temp->next, el);
-		list.size++;
 		return true;
 	}
 }
