d87f2f0e170803d1e12392b3b6a16b63430d7e72
Алсу <salsou96@mail.ru>
2015-05-05 22:12:56 +0500

    Create list_12.pas

---
+++ homeworks/Sharafutdinova/exam/list/list_12.pas
@@ -0,0 +1,68 @@
+uses
+  twolinklist;
+
+const
+  n = 10;
+
+var
+  l1, l2, l3: list;
+
+procedure fill(var l: list);
+var
+  i, m: integer;
+begin
+  readln(m);
+  create(l, m);
+  for i := 1 to n do 
+  begin
+    readln(m);
+    in_end(l, m);
+  end;
+end;
+
+function union(l1, l2: list): list;
+var
+  l3: list;
+begin
+  create(l3, 0);
+  l1.current := l1.head^.next;
+  l2.current := l2.head^.next;
+  while ((l1.current^.next <> nil) and (l2.current^.next <> nil)) do 
+  begin
+    if l1.current^.data = l2.current^.data then begin
+      in_end(l3, l1.current^.data);
+      next(l2);
+      next(l1);
+    end else
+    if l1.current^.data < l2.current^.data then begin
+      in_end(l3, l1.current^.data);
+      next(l1)
+    end else begin
+      in_end(l3, l2.current^.data); 
+      next(l2);
+    end;
+  end;
+  //если один из списков закончился, оставшиеся элементы второго записываем в конец
+  if l2.current^.next <> nil then while l2.current^.next <> nil do 
+    begin
+      in_end(l3, l2.current^.data);
+      next(l2);
+    end;
+  if l1.current^.next <> nil then while l1.current^.next <> nil do 
+    begin
+      in_end(l3, l1.current^.data);
+      next(l1);
+    end;
+  l3.current := l3.head^.next;
+  del_cur(l3);
+  result := l3;
+end;
+
+begin
+  fill(l1);
+  fill(l2);
+  print(l1);
+  print(l2);
+  l3 := union(l1, l2);
+  print(l3);
+end.
